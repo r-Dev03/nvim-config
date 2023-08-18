2d8e97a90ea6ed59a9b3e806b150746b80bf04da
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-08-18 16:48:32 -0500

    Added two new remaps.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,3 +6,11 @@ vim.g.mapleader = " "
 -- Read `:help mapping` and `:help vim.keymap.set`
 vim.keymap.set("n", "<Leader>e", vim.cmd.Ex)
 vim.keymap.set("i", "<C-BS>", "<Esc>cvb", {})
+
+-- Moving a block of code using C/V in visual mode with movement keys
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Let cursor remain at current spot after appending a line using J 
+vim.keymap.set("n", "J", "mzJ`z")
+
