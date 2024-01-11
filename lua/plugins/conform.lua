b42e2aee4920baf10ea0c1a3a0f61a1e7223ac96
Ribbal Hussain <ahmedhussain32000@gmail.com>
2024-01-11 09:08:26 -0600

    Deleted none ls and installed conform nvim plugin. Setup to format using eslintd and other formatters. Working properly now.

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,23 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+		require("conform").setup({
+			formatters_by_ft = {
+				c = { "clang_format" },
+				javascript = { "eslint_d", "prettierd" },
+				typescript = { "prettierd" },
+				markdown = { "prettierd" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
+				sh = { "beautysh" },
+				lua = { "stylua" },
+				python = { "black", "isort" },
+				rust = { "rustfmt" },
+			},
+		})
+		vim.keymap.set("n", "<leader>f", conform.format)
+	end,
+}
