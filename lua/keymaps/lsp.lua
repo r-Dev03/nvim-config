3d2b6d4869a95d207d5111f419efd939a365aa77
Ribbal Hussain <ahmedhussain32000@gmail.com>
2024-02-16 20:35:19 -0600

    Resolved error with LSP toggler now confirmed working

--- lua/keymaps/lsp.lua
+++ lua/keymaps/lsp.lua
@@ -6,8 +6,10 @@ vim.keymap.set("n", "<leader>ut", function()
 	local tailwind_id = toggler.get_lsp_num("tailwindcss")
 	if (tailwind_id == 0) then
 		toggler.start_server("tailwindcss")
+		vim.notify("Tailwind LSP started", "info", { id  = tailwind_id })
 	else
 		toggler.stop_server(tailwind_id)
+		vim.notify("Tailwind LSP stopped", "info", { id  = tailwind_id })
 	end
 end, { desc = "Toggle Tailwind LSP Active / Inactive" })
 return M
