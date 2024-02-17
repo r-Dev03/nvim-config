eebb0925f2195c223f6213717fd56acc141f2f58
Ribbal Hussain <ahmedhussain32000@gmail.com>
2024-02-16 21:29:04 -0600

    Altered mode section in lualine to be a substring, to only show initial character

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -23,12 +23,33 @@ return {
 					},
 				},
 				sections = {
-					lualine_a = { "mode" },
-					lualine_b = { "branch", "diff", "diagnostics" },
-					lualine_c = { "filename" },
-					lualine_x = { "encoding", "fileformat", "filetype" },
-					lualine_y = { "progress" },
-					lualine_z = { "location" },
+					lualine_a = {
+						{
+							"mode",
+							fmt = function(str)
+								return str:sub(1, 1)
+							end,
+						},
+					},
+					lualine_b = {
+						"branch",
+						"diff",
+						"diagnostics",
+					},
+					lualine_c = {
+						"filename",
+					},
+					lualine_x = {
+						"encoding",
+						"fileformat",
+						"filetype",
+					},
+					lualine_y = {
+						"progress",
+					},
+					lualine_z = {
+						"location",
+					},
 				},
 				inactive_sections = {
 					lualine_a = {},
