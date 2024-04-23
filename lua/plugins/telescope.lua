fc57f145e224542dcef56bcfd51e09530dfc7fdd
Ribbal Hussain <ahmedhussain32000@gmail.com>
2024-04-22 22:11:14 -0500

    Removed vim-tpipeline in favor of new tmux status bar at the top. Added new option for telesecope find file keymap to also search for hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -69,7 +69,10 @@ return {
 		-- vim.api.nvim_set_hl(0, "TelescopeSelection", { link = "Visual" })
 		--
 		-- See `:help telescope.builtin`
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+		vim.keymap.set("n", "<leader>ff",
+			function ()
+				builtin.find_files{hidden = true} end,
+			{ desc = "[F]ind [F]iles" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep or words" })
 		vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Search [G]it [F]iles" })
 		vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "[F]ind active [B]uffers" })
