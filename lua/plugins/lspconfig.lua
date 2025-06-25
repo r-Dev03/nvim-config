be4dc867b6bf5dfc5e807be070a790c7025e20c1
Ribbal Hussain <ribbalh0@gmail.com>
2025-06-25 17:12:55 -0500

    updated lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,11 +5,9 @@ return {
 		local win = require("lspconfig.ui.windows")
 		win.default_options.border = "rounded"
 
-		lspconfig.tsserver.setup({})
+		lspconfig.ts_ls.setup({})
 
 		lspconfig.nixd.setup({})
-
-		lspconfig.typst_lsp.setup({})
 
 		lspconfig.clangd.setup({})
 
