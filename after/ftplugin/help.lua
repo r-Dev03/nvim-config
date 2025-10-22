efe2a1ea8bee55b31a8fcddadcd405577381f4ec
Ribbal Hussain <ribbalh0@gmail.com>
2025-10-22 18:45:49 -0500

    minor refactor

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,1 +1,5 @@
-vim.cmd.wincmd("L")
+-- Make help open full screen
+vim.cmd.wincmd("_")
+
+-- Close buffer with q
+vim.keymap.set("n", "q", ":bd<cr>", { buffer = 0 })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,25 +12,14 @@ return {
 		local builtin = require("telescope.builtin")
 
 		telescope.setup({
-			extensions = {},
 			defaults = {
 				dynamic_preview_title = true,
-				vimgrep_arguments = {
-					'rg',
-					'--with-filename',
-					'--no-heading',
-					'--line-number',
-					'--column',
-					'--ignore-case',
-					-- '--hidden',
-				},
-				file_ignore_patterns = {
-					"node_modules",
-					".git/",
-				},
 				path_display = { truncate = 2 },
+				layout_config = {
+					horizontal = { width = 0.9, preview_width = 0.7 },
+				},
 				prompt_prefix = "   ",
-				selection_caret = "  ",
+				selection_caret = "󱞩 ",
 				entry_prefix = "   ",
 				multi_icon = " ",
 				mappings = {
@@ -44,21 +33,45 @@ return {
 						["<C-s>"] = actions.file_vsplit,
 					},
 				},
-				layout_config = {
-					horizontal = { width = 0.9, preview_width = 0.7 },
+
+				vimgrep_arguments = {
+					"rg",
+					"--with-filename",
+					"--no-heading",
+					"--line-number",
+					"--column",
+					"--ignore-case",
+				},
+
+				file_ignore_patterns = {
+					"node_modules",
+					"%.direnv/",
+					"%.git/",
+					".*%.lock/",
+				},
+			},
+
+			pickers = {
+				find_files = {
+					prompt_title = "Find Files",
+					find_command = {
+						"fd",
+						"--hidden",
+						"--exclude=.sqlx",
+						"--exclude=*.lock",
+						"--exclude=target",
+						"--exclude=build",
+					},
 				},
 			},
 		})
 		telescope.load_extension("fzf")
 
-		vim.keymap.set("n", "<leader>ff", function()
-			builtin.find_files({ hidden = true })
-		end, { desc = "[F]ind [F]iles" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
 		vim.keymap.set("n", "<leader>fl", builtin.live_grep, { desc = "[F]ind by [L]ive grep" })
 		vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Search [G]it [F]iles" })
 		vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "[F]ind active [B]uffers" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind help tags" })
-		vim.keymap.set("n", "<leader>fd", ":Telescope find_files find_command=find,-type,d,!,-name,'node_modules'<cr>", { desc = "[F]ind [D]irectories" })
 		vim.keymap.set("n", "<leader>fi", ":Telescope highlights <CR>", { desc = "[F]ind highl[I]ghts" })
 		vim.keymap.set("n", "<leader>f/", builtin.current_buffer_fuzzy_find, { desc = "[/] Fuzzily [F]ind in current buffer" })
 	end,
