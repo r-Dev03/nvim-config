9a93991a0c59e5f972358de130a39abdb79052de
Ribbal Hussain <ribbalh0@gmail.com>
2025-10-26 19:45:22 -0500

    removed custom mappings for now.

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -1,14 +1,2 @@
-local gitmap = function(lhs, rhs)
-	return vim.keymap.set("n", lhs, ":Git " .. rhs, { buffer = true })
-end
-
-gitmap("gl", "log --oneline")
-gitmap("gL", "log --graph")
-gitmap("gc", "commit -v")
-gitmap("gf", "fetch --all")
-gitmap("gp", "push")
-gitmap("gb", "blame")                                   -- blame view
-gitmap("ga", "add %")
-gitmap("gd", "diff")
 vim.keymap.set("n", "q", ":quit<CR>", { buffer = true, silent = true })
 
